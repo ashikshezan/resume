7573c5c534bda9f28d648a669d4968505a23826f
ashikshezan <ashikshezan@gmail.com>
2021-02-07 20:01:37 +0600

    rendered pdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Resume from './Resume';
+
+import { Document, Page } from 'react-pdf';
+import pdf from './ashik_shezan_resume_v2.pdf'
+import { useState } from 'react';
 
 function App() {
+  function removeTextLayerOffset() {
+    const textLayers = document.querySelectorAll(".react-pdf__Page__textContent");
+    textLayers.forEach(layer => {
+      const { style } = layer;
+      style.top = "0";
+      style.left = "0";
+      style.transform = "";
+    });
+  }
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div >
+      <div style={{ width: 800, height: "100%" }}>
+        <Document
+          file={pdf}
+          onLoadSuccess={onDocumentLoadSuccess}
         >
-          Learn React
-        </a>
-      </header>
+          <Page pageNumber={pageNumber} width={800} onLoadSuccess={removeTextLayerOffset} />
+        </Document>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
